0ab06271656f3c80fec88d051a5ada3c89833525
Michael Dawson <michael_dawson@ca.ibm.com>
2015-12-04 17:16:25 -0600

    Add logic to pull data from database for chart

--- experimental/chartGen/createpage.js
+++ experimental/chartGen/createpage.js
@@ -1,18 +1,47 @@
 var fs = require('fs');
+var mysql = require('mysql');
 
 var createData = function(data) {
   var htmlDataArray = [];
 
   for (i = 0; i < data.length; i++) {
-    htmlDataArray[i] =  '  data.push({\n    x: new Date('+ data[i][0] + '),\n    y: ' + data[i][1] + '\n  });\n'; 
+    htmlDataArray[i] =  '  data.push({\n    x: new Date('+ data[i].time * 1000 + '),\n    y: ' + data[i].value + '\n  });\n'; 
   }
   return htmlDataArray.join("");
 }
 
-// replace this by pulling data from database
-var data = [[Date.parse("2015-04-27T04:00:00+03:00"),343],[Date.parse("2015-04-29T04:00:00+03:00"),12312]];
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'XXXXXX',
+  password: 'XXXXXXXXXX',
+  database: 'benchdb'
+})
 
-// put the data into the page template
-var mainPage = fs.readFileSync('page.html.base').toString();
-mainPage = mainPage.replace('<DATA_ELEMENTS>', createData(data));
-fs.writeFileSync('page.html', mainPage);
+
+con.connect(function(err) {
+  if (err) {
+    console.log('failed to connect to db:' + err);
+    return;
+  }
+  
+  con.query('SELECT time,value FROM benchresults WHERE streamid=1 AND benchid=1', function(err, rows, fields) {
+    if (err) {
+      console.log('failed to run query db:' + err);
+      return;
+    }
+
+   // put the data into the page template
+   var mainPage = fs.readFileSync('page.html.base').toString();
+   mainPage = mainPage.replace('<DATA_ELEMENTS>', createData(rows));
+   fs.writeFileSync('page.html', mainPage);
+ 
+    con.end(function(err) {
+      if (err) {
+        console.log('failed to cleanly close db connection:' + err);
+        return;
+      }
+    });
+  });
+});
+
+
