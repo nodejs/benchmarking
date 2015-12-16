fe1771919166d5d768188b7087e9972cd7e53cd3
Michael Dawson <michael_dawson@ca.ibm.com>
2015-12-16 17:23:24 -0500

    Updated to support streamid

--- experimental/postit.sh
+++ experimental/postit.sh
@@ -1,3 +1,3 @@
-CUR_TIME=`date +%s`
-wget -O result --user=xxxxxxxxx --password=xxxxxxxxx --no-check-certificate  "https://10.52.6.151:3000?benchid=1&time=$CUR_TIME&value=$1"
+UR_TIME=`date +%s`
+wget -O result --user=xxxxxxx --password=xx --no-check-certificate  "https://10.52.6.151:3000?streamid=$1&benchid=$2&time=$CUR_TIME&value=$3" >/dev/null 2>&1
 cat result
